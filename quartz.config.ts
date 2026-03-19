b1a8916c60ffd483479ffe9cf8c9569b2d87a0f6
victoriemini <victoriemini@gmail.com>
2026-03-19 14:23:00 -0300

    Quartz sync: Mar 19, 2026, 2:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "victoriemini",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://victoriemini.github.io/dataBASE",
+    baseUrl: "victoriemini.github.io/dataBASE",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
